0f437c8fbbcc5e3c2ac04b7587861c0dea70b390
Abdelmounim Rihane <abdelmounaimrihan@gmail.com>
2023-09-27 23:32:15 +0100

    Add project files.

---
+++ TestCoreApp/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestCoreApp.Models;
+
+namespace TestCoreApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options ):base(options)
+        {
+                
+        }
+
+        public DbSet<Item> Item { get; set; }
+    }
+}
---
+++ TestCoreApp/Models/Item.cs
@@ -0,0 +1,20 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace TestCoreApp.Models
+{
+    public class Item
+
+         
+    {
+        public int  Id {  get; set; }
+        [Required]
+        public string name   { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+
+        public DateTime Created { get; set; } =DateTime.Now;
+        
+        
+     }
+}
